985dbfc419c881d0489c81139904616a51089067
Evgeny Zaitsev <evgeny_zaitsev@epam.com>
2016-07-07 10:39:12 +0300

    No Hardcode

--- HW_5/HW_5/Program.cs
+++ HW_5/HW_5/Program.cs
@@ -7,7 +7,9 @@ namespace HW_5
     {
         static void Main(string[] args)
         {
-            int n = 3;
+            Console.WriteLine("Enter n and program will count time for 1000, 10000, ..., 10^(n+3) elements.");
+            int n;
+            n = Int32.Parse(Console.ReadLine());
             string tempPath = $"{Data.path}_{DateTime.Now.ToString("dd-MM-yy_HH-mm-ss")}.txt";
             if (!File.Exists(tempPath))
             {
